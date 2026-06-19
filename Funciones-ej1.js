a9a07cbeed45db7ad34e73cd8d450d1fd2b8b42e
M4t-ez <m4t.ez.mz@gmail.com>
2026-06-19 20:05:06 -0300

    Add files via upload

---
+++ Funciones-ej1.js
@@ -0,0 +1,52 @@
+const leer = require("prompt-sync")();
+function main() {
+    const personajes = [
+        "Harry Potter",
+        "Hermione Granger",
+        "Ron Weasley",
+        "Albus Dumbledore",
+        "Severus Snape",
+        "Lord Voldemort",
+        "Draco Malfoy",
+        "Sirius Black",
+        "Rubeus Hagrid",
+        "Luna Lovegood"
+    ];
+    let opcion;
+    let indiceImpostor;
+    let impostor;
+
+    mostrarPersonajes(personajes);
+
+    opcion = Number(leer("    "));
+
+    indiceImpostor = generarImpostor(personajes);
+    impostor = personajes[indiceImpostor];
+
+    console.log("");
+    informarResultado(opcion, indiceImpostor, impostor);
+}
+
+function mostrarPersonajes(personajes) {
+    console.log("Selecciona un número de personaje para encontrar al impostor:");
+    for (let i = 0; i < personajes.length; i++) {
+        console.log("    " + (i + 1) + " - " + personajes[i]);
+    }
+}
+
+function generarImpostor(personajes) {
+    return Math.floor(Math.random() * personajes.length);
+}
+
+function informarResultado(opcionElegida, indiceImpostor, nombreImpostor) {
+    if (opcionElegida - 1 === indiceImpostor) {
+        console.log("¡Adivinaste! El impostor es: " + nombreImpostor);
+    } else {
+        console.log("¡Oh no! No has adivinado el impostor. Algo misterioso está ocurriendo...");
+        console.log("Cuidado con " + nombreImpostor + "!");
+    }
+}
+
+
+
+main();
